27728271e42f693d2d776a34a354905bca566029
NickHaecker <nick.philipp.haecker@hs-furtwangen.de>
2020-11-08 13:03:03 +0100

    finished this shit

--- Program.cs
+++ Program.cs
@@ -8,18 +8,15 @@ namespace Projekt
     {
         static void Main(string[] args)
         {
-            // Create a list of parts.
             List<Pokemon> pokemons = new List<Pokemon>();
             List<Trainer> trainer = new List<Trainer>();
-            // Add parts to the list.
+
             pokemons.Add(new Pokemon(0, false, new List<int> { 1, 0, 2 }, -1));
             pokemons.Add(new Pokemon(1, false, new List<int> { 2, 1, 0 }, -1));
-            pokemons.Add(new Pokemon(2, false, new List<int> { 0, 2, 1 }, -1));
-            trainer.Add(new Trainer(0, false, new List<int> { 1, 0, 2 }, -1));
+            pokemons.Add(new Pokemon(2, false, new List<int> { 2, 1, 0 }, -1));
+            trainer.Add(new Trainer(0, false, new List<int> { 2, 1, 0 }, -1));
             trainer.Add(new Trainer(1, false, new List<int> { 1, 2, 0 }, -1));
             trainer.Add(new Trainer(2, false, new List<int> { 0, 2, 1 }, -1));
-            // Console.WriteLine(pokemons);
-            // Console.WriteLine(trainer);
 
             Maschine(pokemons, trainer);
 
@@ -44,29 +41,15 @@ namespace Projekt
             {
                 Console.WriteLine("-----start while-----");
                 int t;
-
-
                 for (t = 0; t < trainer.Count; t++)
                 {
-                    Console.WriteLine("t{0}" + t);
-                    // Console.WriteLine("step3");
-                    if (trainer[t].matched == true)
-                    {
-                        Console.WriteLine("# Trainer already matched! ");
-                        break;
-                    }
+                    Console.WriteLine("t" + t);
 
-                    for (int f = 0; f < trainer[t].favourites.Count; f++)
+                    if (trainer[t].matched == false)
                     {
-                        // if (trainer[t].favourites[f] == -1)
-                        // {
-
+                        int f = 0;
                         int favouritePokemon = trainer[t].favourites[f];
                         Console.WriteLine("Fav Pok id: {0}", favouritePokemon);
-
-
-                        // if (trainer[t].favourites[f] != -1)
-                        // {
                         if (pokemon[favouritePokemon].matched == false)
                         {
                             Console.WriteLine("Matchig erfolgreich! FreeTrainer -1 ");
@@ -75,9 +58,9 @@ namespace Projekt
                             pokemon[favouritePokemon].matched = true;
                             pokemon[favouritePokemon].matchedId = trainer[t].id;
                             freeTrainer--;
-                            Console.WriteLine("Trainer {0} " + trainer[t].id + " Pokemon {0} = " + trainer[t].matchedId);
-                            Console.WriteLine("FreeTrainer:{0}", freeTrainer);
-                            // break;
+
+                            Console.WriteLine("Trainer " + trainer[t].id + " Pokemon  = " + trainer[t].matchedId);
+                            Console.WriteLine("FreeTrainer:{0} -1", freeTrainer);
                         }
                         else
                         {
@@ -94,54 +77,50 @@ namespace Projekt
                                 trainer[t].matchedId = pokemon[favouritePokemon].id;
                                 pokemon[favouritePokemon].matchedId = trainer[t].id;
                                 pokemon[favouritePokemon].matched = true;
-                                Console.WriteLine("Trainer {0} " + trainer[t].id + " Pokemon {0} = " + trainer[t].matchedId);
-
-                                Console.WriteLine("-----! Ende Zyklus !------");
-
-                                // break;
+                                Console.WriteLine("Trainer" + trainer[t].id + " Pokemon= " + trainer[t].matchedId);
                             }
-                            // break;
                         }
-                        Console.WriteLine("START PURGE ! :)");
-                        Console.WriteLine("gelöscht wird pokemon: {0}" + trainer[t].favourites[0] + " von Trainer: {0}" + trainer[t]);
-                        trainer[t].favourites.RemoveAt(f);
-                        // if (t < 2)
-                        // {
-                        //     t += 1;
-                        // }
+                        Console.WriteLine("gelöscht wird pokemon:" + trainer[t].favourites[f] + " von Trainer:" + trainer[t].id);
+                        trainer[t].favourites.RemoveAt(0);
 
-                        // break;
-
-
-                        // trainer[t].favourites.SetValue(-1, f);
-                        // if (trainer[t].matched == true)
-                        // {
-                        // for (int g = 0; g == f; g++)
-                        // {
-                        //     trainer[t].favourites.SetValue(-1, g);
-                        //     Console.WriteLine("START PURGE ! :)");
-                        //     // break;
-
-                        // }
-                        // }
-
-                        // }
-                        // for (int g = 0; g <= f; g++)
-                        // {
-                        // trainer[t].favourites = -1;
-                        // break;
-                        // }
-                        // break;
+                        Console.WriteLine("-----! Ende Zyklus !------");
                     }
-                    if (t == 2)
+                    else
                     {
-                        Console.WriteLine("reset");
-                        t = -1;
+                        Console.WriteLine("# Trainer is already matched !");
                     }
                 }
             }
             Console.WriteLine("-----End of Loop------");
-            Console.WriteLine("-----Ente of Loop------");
+
+            Console.WriteLine("#############");
+            Console.WriteLine("RESULT OF LOOP");
+            Console.WriteLine("#############");
+
+            for (int d = 0; d < trainer.Count; d++)
+            {
+                if (trainer[d].matched)
+                {
+                    Console.WriteLine("Trainer " + trainer[d].id + " is matched with Pokemon " + trainer[d].matchedId);
+                }
+                else
+                {
+                    Console.WriteLine("Trainer " + trainer[d].id + " is unmatched // MatchedID: " + trainer[d].matchedId);
+                }
+            }
+
+            for (int d = 0; d < pokemon.Count; d++)
+            {
+                if (pokemon[d].matched)
+                {
+                    Console.WriteLine("Pokemon " + pokemon[d].id + " is matched with Trainer " + pokemon[d].matchedId);
+                }
+                else
+                {
+                    Console.WriteLine("Pokemon " + pokemon[d].id + " is unmatched // MatchedID: " + pokemon[d].matchedId);
+                }
+            }
+
         }
         private static bool TrainervsTrainer(int currentPokemon, int newTrainer, int oldTrainer, List<Pokemon> pokemon, List<Trainer> trainer)
         {
@@ -158,7 +137,7 @@ namespace Projekt
                     return false;
                 }
             }
-            return false;
+            return true;
         }
     }
 }
